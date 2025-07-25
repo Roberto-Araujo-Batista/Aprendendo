42a768c5abee1627696ece137c289aea3fb9abc3
roberto-araujo-batista <araujoroberto.contato@gmail.com>
2025-07-25 08:48:59 -0300

    aprendendo a criar arquivos

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print('hello, world')
+import openpyxl
+
+#criando arquivo excel
+planilha = openpyxl.Workbook()
+
+#como visualizar pastas existentes
+print(planilha.sheetnames)
+
+#como criar uma página
+planilha.create_sheet('Enviar')
+
+#como selecionar uma página
+enviar = planilha['Enviar']
+
+
+
+#salvar planilha
+planilha.save('dados.xlsx')
