5fd09518c03b7e86ed0bf9ba4ae6c4a643d51324
roberto-araujo-batista <araujoroberto.contato@gmail.com>
2025-07-28 08:35:16 -0300

    primeiro commit

--- app.py
+++ app.py
@@ -16,11 +16,20 @@ enviar.append(['NOME', 'IDADE', 'CARGO'])
 enviar.append(['roberto', '22', 'suporte ti'])
 enviar.append(['gabriel', '5', 'estudante'])
 enviar.append(['william', '9', 'estudante'])
+enviar.append(['clarice', '0', 'bebe'])
 
-
-
-#salvar planilha
+#salvar planilha e fecha arquivo.
 planilha.save('dados.xlsx')
 
 
 
+
+
+
+arquivo = openpyxl.load_workbook('dados.xlsx')
+
+enviar = arquivo['Enviar']
+
+for linha in enviar.iter_rows(): #o openpyxl retorna sempre linha em tuplas com ponteiros para a celula
+    for celula in linha:
+        print(celula.value) #celula é uma referencia a celula da planilha
