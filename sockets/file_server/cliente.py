9548277126f6ced411c8426206369ca336624afb
Roberto Araujo <araujoroberto.contato@gmail.com>
2025-11-27 11:19:08 -0300

    Update cliente.py
    código pronto cliente

--- sockets/file_server/cliente.py
+++ sockets/file_server/cliente.py
@@ -1,71 +1,39 @@
-import socket, os, time
+import socket, os
 
-print('-' * 20 + ' SERVIDOR DISPONÍVEL ' + '-' * 20)
-
-host = ''
+host = '127.0.0.1'
 port = 60000
+servidor = (host, port)
 
+# acoplar o socket da camada de transporte
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.bind((host, port))  # especifica qual porta e ip da máquina receber as solicitações
 
-enviado = False
+nome_arquivo = input('Digite o nome do arquivo: ')
+nome_arquivo = nome_arquivo.encode()
+tamanho_nome = len(nome_arquivo).to_bytes()
 
-while not enviado:
+udp_socket.sendto(tamanho_nome, (servidor))
+udp_socket.sendto(nome_arquivo, (servidor))
 
-    print('Aguardando solicitação...')
+status, src = udp_socket.recvfrom(1)
+status = int.from_bytes(status)
+print(status)
 
-    # recebe primeiro byte(tamanho do nome do arquivo)
-    # recvfrom sempre recebe os bytes e o adrress do solicitante
-    # adress = ip e porta do solicitante
-    tamanho_arquivo, cliente = udp_socket.recvfrom(1)
+if status:
+    print('Arquivo encontrado, recebendo arquivo...')
+
+    tamanho_arquivo, server = udp_socket.recvfrom(4)
     tamanho_arquivo = int.from_bytes(tamanho_arquivo)
 
-    nome_arquivo, cliente = udp_socket.recvfrom(tamanho_arquivo)
-    nome_arquivo = nome_arquivo.decode('utf-8')
+    arquivo = open(nome_arquivo, 'bw')
+    while tamanho_arquivo > 0:
+        dados, server = udp_socket.recvfrom(4096)
+        arquivo.write(dados)
+        tamanho_arquivo = tamanho_arquivo - 4096
+    print('Arquivo salvo com sucesso')
+    arquivo.close()
 
-    print('-' * 20 + ' INFORMAÇÕES DA SOLICITAÇÃO ' + '-' * 20)
-
-    print(f'''
-    nome:               {nome_arquivo}
-    ip cliente:         {cliente[0]}
-    porta cliente:      {cliente[1]}
-    ''')
-
-    status = 0
-    status = status.to_bytes()
-
-    if os.path.isfile(nome_arquivo):
-        status = 1
-        status = status.to_bytes()
-        udp_socket.sendto(status, cliente)
-        print('Arquivo encontrado')
-
-        # mandar tamanho do arquivo, no máximo 4 bytes
-        tamanho_arquivo = os.path.getsize(nome_arquivo).to_bytes(4)
-        udp_socket.sendto(tamanho_arquivo, cliente)
-        tamanho_arquivo = int.from_bytes(tamanho_arquivo)
-
-        print(f'Tamanho do arquivo: {tamanho_arquivo}')
-        # ler o arquivo para mandar
-        try:
-            arquivo = open(nome_arquivo, 'rb')
-            while tamanho_arquivo > 0:
-                enviar = arquivo.read(4096)
-                udp_socket.sendto(enviar, cliente)
-                tamanho_arquivo = tamanho_arquivo - 4096
-            arquivo.close()
-
-        except:
-            print('erro ao abrir arquivo para enviar')
-
-        enviado = True
-
-
-    else:
-        udp_socket.sendto(status, cliente)
-        print('Arquivo não encontrado')
-
-        enviado = True
+else:
+    print('Arquivo não encontrado')
 
 udp_socket.close()
 print('Fim da execução')
