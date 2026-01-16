e114cf9dc758280c02c7896342f75df5fa34f424
Roberto Araujo <araujoroberto.contato@gmail.com>
2026-01-16 12:50:03 -0300

    enquanto trabalho oh kkk
    Added DEBUG mode for message receiving and error handling.

--- sockets/TCP/chat/servidor.py
+++ sockets/TCP/chat/servidor.py
@@ -1,38 +1,44 @@
 import socket, threading
 
+DEBUG = False
+
 def get_ip():
     hostname = socket.gethostname()
     ips      = socket.gethostbyname(hostname)
     return ips
 
 def receive_msg(con, src):
-        #receving len of the message
-        print('receiving the message')
-        msg_len = con.recv(4)
-        msg = con.recv(int.from_bytes(msg_len))
+        try:
+            while True:   
+                #receving len of the message
+                if DEBUG: print('receiving the message')
+                
+                msg_len = con.recv(4)
+                msg = con.recv(int.from_bytes(msg_len))
 
-        print(src[0], src[1], ' sends the following message: \n- ' + msg.decode())
-
-        #sending the message to others users
-        for cli_con, cli_src in ip_list:
-            if cli_con != con:
-                cli_con.send(msg)
+                if DEBUG: print(src[0], src[1], ' sends the following message: \n- ' + msg.decode())
+                #sending the message to others users
+                for cli_con, cli_src in ip_list:
+                    if cli_con != con:
+                        cli_con.send(msg_len)
+                        cli_con.send(msg)
+                        if DEBUG: print('enviado para ', cli_src)
+        except Exception as e:
+            print(e)
 
 def connecting():
     global ip_list
     #connecting the socket
-    print(f'Connecting server by ips: \n{get_ip()}')
+    print(f'Connecting server by ips: {get_ip()}')
 
-    cond = 'True'
-    while cond == 'True':
+    while True:
         con, src = tcp_socket.accept()
-        print(f'Connected with {src[0]}')
+        print(f'Connected with {src[0]} in port {src[1]}')
 
         #putting ips on a list to control the users that keep connected
         ip_list.append((con, src))
 
         threading.Thread(target=receive_msg, args =(con, src)).start()
-
 
 ip_list = []
 
@@ -40,7 +46,4 @@ tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcp_socket.bind(('', 20000))
 tcp_socket.listen(1)
 
-
-threading.Thread(target=connecting).start()
-
-
+connecting()
